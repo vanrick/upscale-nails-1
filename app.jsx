70779acd0d7925e6716e36796b04a730b34d1671
Jonathan Huxhold <jhuxhold@gmail.com>
2016-09-07 14:24:01 -0600

    small tweak to get app working

--- app.jsx
+++ app.jsx
@@ -3,13 +3,11 @@ var ReactDOM = require('react-dom');
 
 import { Router, Route, browserHistory } from 'react-router';
 import Home from './app/components/home/index';
-import Home from './app/components/home/second';
 import './assets/style.css';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={Home} />
-    <Route path="/second" component={Second} />
   </Router>,
   document.getElementById('app')
 );
