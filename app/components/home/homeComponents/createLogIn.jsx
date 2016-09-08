73acc8a17a28951b6f5c0e3f530578d8460a3725
Rick Van <x.rckvan@gmail.com>
2016-09-08 11:49:14 -0600

    login text form complete

---
+++ app/components/home/homeComponents/createLogIn.jsx
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+export default class CreateLoginForm extends Component {
+  render() {
+    return (
+      <div>
+        
+        <h1>Create Login</h1>
+      </div>
+    )
+  };
+}
