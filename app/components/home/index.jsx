209c4427670d9b60389305909c5e7a5f3ebf42a5
MIchael <michael.olson756@gmail.com>
2016-09-08 09:38:17 -0600

    changes to public bundle

--- app/components/home/index.jsx
+++ app/components/home/index.jsx
@@ -1,12 +1,32 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
+import { Route, RouteHandler, Link } from 'react-router';
+import AuthService from '../services/AuthService'
+import { Button, Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class Home extends Component {
   render() {
-    return <h1>Unique Nails, A Place for friends!!!</h1>
+    return <h1>hello</h1>
+    <Nav pullRight>
+        <NavItem eventKey={1}>Home</NavItem>
+        <NavItem eventKey={2}>Book Inv</NavItem>
+        <NavDropdown eventKey={3} title="Authorization" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Logout</MenuItem>
+        </NavDropdown>
+    </Nav>
   }
 };
 
 Home.contextTypes = {
   router: React.PropTypes.object.isRequired,
 }
+
+
+
+
+
+
+
+
+/// In the render() method
