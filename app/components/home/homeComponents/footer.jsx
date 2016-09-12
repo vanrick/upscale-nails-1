e306ea04a71117489e652565e3acc76e4d2514af
MIchael <michael.olson756@gmail.com>
2016-09-12 13:25:56 -0600

    pull from master

--- app/components/home/homeComponents/footer.jsx
+++ app/components/home/homeComponents/footer.jsx
@@ -24,8 +24,7 @@ export default class Footer extends Component {
           </Row>
         </Grid>
       </div>
-
-      </div>
+    </div>
     )
   }
 }
--- app/components/home/homeComponents/userReviews.jsx
+++ app/components/home/homeComponents/userReviews.jsx
@@ -22,7 +22,7 @@ export default class UserReviews extends Component {
           </Grid>
         </div>
 
-        
+
 
     </div>
     )
